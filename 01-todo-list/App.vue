7e00b62bacccb1a5951ab3e4848cc1babae3d646
Kodak <special.moucom@gmail.com>
2019-01-14 00:26:22 +0900

    [add]todo-list

---
+++ 01-todo-list/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <!-- ここに処理を書いていく -->
+        <h2>タスク一覧</h2>
+        <ul>
+            <li v-for="task in tasks" v-bind:key="task.id">
+                <input type="checkbox" v-bind:checked="task.done">
+                {{task.name}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    // ここに実装を書いていく
+    computed: {
+        tasks() {
+            // ストアを読む
+            return this.$store.state.tasks
+        }
+    }
+}
+</script>
