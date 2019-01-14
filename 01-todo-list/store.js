75b42e04b4fbe79602f5bab4e23c2158f8b65d42
Kodak <special.moucom@gmail.com>
2019-01-15 01:04:23 +0900

    [update]01-todo-list

--- 01-todo-list/store.js
+++ 01-todo-list/store.js
@@ -12,14 +12,91 @@ const store = new Vuex.Store({
             {
                 id: 1,
                 name: '牛乳を買う',
+                labelIds: [1, 2],
                 done: false
             },
             {
                 id: 2,
                 name: 'Vue.jsの本を買う',
+                labelIds: [1, 3],
                 done: true
             }
-        ]
+        ],
+
+        // ラベルの初期ステート
+        labels: [
+            {
+                id: 1,
+                text: '買い物'
+            },
+            {
+                id: 2,
+                text: '食料'
+            },
+            {
+                id: 3,
+                text: '本'
+            }
+        ],
+        // 次に追加するタスクのID
+        // 実際のアプリではサーバーで生成したり、UUIDを使ったりするがここは決め打ち
+        // UUID = Universally Unique Identifier(普遍的ユニークID)
+        nextTaskId: 3,
+        nextLabelId: 4,
+
+        // フィルタするラベルのID
+        filter: null
+    },
+    getters: {
+        // フィルタあとのタスクを返す
+        filteredTasks(state) {
+            // ラベルが選択されていなければそのままの一覧を返す
+            if(!state.filter) {
+                return state.tasks
+            }
+
+            // 選択されているラベルでフィルタリングする
+            return state.tasks.filter(task => {
+                return task.labelIds.indexOf(state.filter) >= 0
+            })
+        }
+    },
+    mutations: {
+        // タスクを追加
+        addTask(state, {name, labelIds}) {
+            state.tasks.push({
+                id: state.nextTaskId,
+                name,
+                labelIds,
+                done: false
+            })
+
+            // 次に追加されるタスクに付与するIDを更新
+            state.nextTaskId++
+        },
+        // タスクの完了状態を変更する
+        toggleTaskStatus(state, {id}) {
+            const filtered = state.tasks.filter(task => {
+                return task.id === id
+            })
+
+            filtered.forEach(task => {
+                task.done = !task.done
+            })
+        },
+        // ラベルを追加する
+        addLabel(state, {text}) {
+            state.labels.push({
+                id: state.nextLabelId,
+                text
+            })
+            // 次に追加されるラベルに付与するIDを更新
+            state.nextLabelId++
+        },
+        // フィルタリング対象のラベルを変更する
+        changeFilter(state, {filter}){
+            state.filter = filter
+        }
     }
 })
 
