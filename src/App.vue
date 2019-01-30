fc749b1d3937d8295c70b6d3e024c24e737f66d9
Kodak4400 <special.moucom@gmail.com>
2019-01-30 19:38:33 +0900

    [add]tmp

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+      <game scores="scores"></game>
+      <score scores="scores"></score>
+  </div>
+</template>
+
+<script>
+import Game from '@/components/Game.vue'
+import Score from '@/components/Score.vue'
+
+export default {
+  name: 'App',
+  
+  components: {
+      Game,
+      Score
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
