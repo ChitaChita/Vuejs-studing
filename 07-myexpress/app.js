0abbfd75f378271d2c85d895c5ad93cc1152de7b
Kodak <special.moucom@gmail.com>
2019-02-15 00:39:48 +0900

    [update]dynamodb-localを使ってみる（未完）

--- 07-myexpress/app.js
+++ 07-myexpress/app.js
@@ -1,5 +1,41 @@
+// aws dynamodb scan \
+// --endpoint-url http://localhost:8000 \
+// --table-name Music
+
+
 const express = require('express')
 const app = express()
+
+const AWS = require('aws-sdk');
+AWS.config.update({
+    endpoint : 'http://localhost:8000', // エンドポイント
+    region : 'ap-northeast-1'
+});
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const TABLE_NAME = 'Music';
+
+// DynamoDB scanで必要なパラメータ
+let params = {
+    TableName : 'Music' // テーブル名
+};
+
+
+// scanの実行(非同期で実行され、コールバックで結果を受ける)
+docClient.scan(params, function(err, data) {
+    if (err) {
+        // エラー時の処理を記述
+        console.log("エラー = " + err);
+    } else {
+        // 成功時の処理を記述(取得結果はdataに格納される)
+        console.log("成功 = " + JSON.stringify( data ));
+        data.Items.forEach(dt => {
+            console.log(dt.Artist);
+            console.log(dt.SongTitle);
+            console.log(dt.AlbumTitle);
+        });
+    }
+});
 
 app.get('/', (req, res) => res.send('Hellow World'))
 
