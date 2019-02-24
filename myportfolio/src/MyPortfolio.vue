57feee82a07a6bba091f7e27b975aa18ad4f55ae
Kodak <special.moucom@gmail.com>
2019-02-24 20:52:08 +0900

    [add]myportfolio first commit.

---
+++ myportfolio/src/MyPortfolio.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="myportfolio">
+    <nav class="global-nav clearfix">
+      <ul>
+        <li class="nav-item"><a href="#">Home</a></li>
+        <li class="nav-item"><a href="#">About</a></li>
+        <li class="nav-item"><a href="#">Work</a></li>
+      </ul>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyPortfolio'
+}
+</script>
+
+<style>
+#myportfolio {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.global-nav {
+  text-align: center;
+}
+.global-nav .nav-item {
+  display: inline-block;
+  float: left;
+  margin-left: -1px;
+}
+.global-nav {
+  width: 50px;
+  height: 40px;
+  font-size: 1.4rem;
+  line-height: 15px;
+  text-align: center;
+  /* フレックスコンテナによる中央寄せ */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.global-nav .nav-item a {
+  display: inline-block;
+  width: 120px;
+  height: 40px;
+  border: 1px solid gainsboro;
+  font-size: 1.4rem;
+  line-height: 40px;
+  text-align: center;
+  letter-spacing: 1px;
+  transition: 0.15s;
+  color: black;
+  font-weight: bold;
+}
+.global-nav .nav-item.active a,
+.global-nav .nav-item a:hover {
+  background-color: orange;
+  color: black;
+}
+</style>
---
+++ myportfolio/src/components/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <h1>Try T.M Engineer Portfolio</h1>
+    <svg viewbox="0 0 300 300" width="300" height="300">
+      <circle :r="r" :cx="cx" :cy="cy" stroke="black" />
+    </svg>
+    <input type="range" v-model="cx">
+    <input type="range" v-model="cy">
+    <input type="range" v-model="r">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      cx: 180,
+      cy: 60,
+      r: 50
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
