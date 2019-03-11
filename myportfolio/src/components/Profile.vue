21a93e6fea095585b9f6b934f3bbe958d291bddf
Kodak <special.moucom@gmail.com>
2019-03-12 00:37:35 +0900

    [add]ProfileとWorksの追加

---
+++ myportfolio/src/components/Profile.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="profile">
+    <h1>Profile</h1>
+    <img src="@/assets/Kodak.png" border="0">
+    <div>
+      多摩市在住のバックエンドエンジニア。<br>
+      約11年間、客先常駐でマネジメントを続けてきて、技術力が全く無くて後悔していた人。<br>
+      34歳でWebエンジニアへ転職して、技術力を身につけて行こうと決意。<br>
+    </div>
+    <h2>Development Skills</h2>
+    <div>
+      Front: JavaScript(ES2015+) / FW(Vue.js, Riot.Js) / CSS(Sass)<br>
+      ServerSide: Node.js / Java / Cloud(AWS, GCP)<br>
+      Database: / MongoDB / PostgreSQL<br>
+      資格：基本情報 / ソフトウェア開発技術者 / ネットワーークスペシャリスト / セキュリティスペシャリスト<br>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
---
+++ myportfolio/src/components/Works.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="profile">
+    <h1>Works</h1>
+    <div class="works-box">
+        <p class="works-title">Potoment</p>
+        <img class="works-img" src="#">
+        <p class="works-desc">初めて個人作成した成果物。LINE Message APIとRubyを使って写真共有サービスを作りました。詳細はブログを参照ください。</p>
+        <a href="https://kodak.hatenablog.com/entry/2018/04/08/111954">リンク</a>
+    </div>
+    <div>
+        <p>Try T.M Engineer Portfolio</p>
+        <p>本サイトのこと。</p>
+        <a href="https://kodak.hatenablog.com/entry/2018/04/08/111954">リンク</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+.works-box {
+    display: block;
+    background-color: lightcoral;
+    width: 200px;
+    height: 250px;
+    border-radius: 10px
+}
+.works-title {
+    display: block;
+    background-color: yellow;
+    font-size: 1.2em;
+    margin: 0px auto;
+}
+.works-img {
+    display: block;
+    background-color: blue;
+    width: 200px;
+    height: 130px;
+    margin: 3px auto;
+}
+.works-desc {
+    display: block;
+    background-color: lightgrey;
+    font-size: 0.8em;
+    margin: 0px auto;
+    text-align: left;
+}
+</style>
